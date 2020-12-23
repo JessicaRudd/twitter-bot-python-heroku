396d301fe6818e1cd73f604414aa1a58325de383
Jessica M. Rudd <74568718+jrudd1@users.noreply.github.com>
2020-12-23 15:23:42 -0500

    Update server.py

--- server.py
+++ server.py
@@ -1,5 +1,12 @@
 from os import environ
 from flask import Flask
+import twitter_bot
 
 app = Flask(__name__)
+
+@app.route("/")
+def home():
+    twitter_bot.tweet_quote()
+    return "Tweeting weather and a quote..."
+
 app.run(host='0.0.0.0', port=environ.get('PORT'))
